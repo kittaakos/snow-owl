4e041d1f063c875eb0ca7a3840fe5b1fe8a79d61
Mark Czotter <mczotter@b2international.com>
2015-07-15 23:23:35 +0200

    SO-1639: refactored testcases of DefaultTransactionalIndexTest
    Also added deletion test cases.

--- core/snowowl-core-tests/src/com/b2international/snowowl/core/store/index/tx/DefaultTransactionalIndexTest.java
+++ core/snowowl-core-tests/src/com/b2international/snowowl/core/store/index/tx/DefaultTransactionalIndexTest.java
@@ -45,9 +45,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @since 5.0
  */
 public class DefaultTransactionalIndexTest extends PersonFixtures {
-	
+
 	private static final String COMMIT_MESSAGE = "Commit Message XXX";
-	
+
 	@Rule
 	public ESRule es = new ESRule();
 
@@ -62,19 +62,19 @@ public class DefaultTransactionalIndexTest extends PersonFixtures {
 	private Person person1_2015;
 	private Person person1_YobChanged;
 	private Person person1_FirstNameChanged;
-	
+
 	@Before
 	public void givenTransactionalIndex() {
 		this.mapper = new DefaultObjectMapper();
 		this.manager = new VisibleInBranchManagerImpl(store, clock);
 		this.main = manager.getMainBranch();
-		
+
 		this.txIndex = createTransactionalIndex();
 		this.txIndex.admin().delete();
 		this.txIndex.admin().create();
-		
+
 		this.manager.setIndex(txIndex);
-		
+
 		this.person1_2015 = createPerson1();
 		this.person1_YobChanged = createPerson1();
 		this.person1_YobChanged.setYob(1997);
@@ -83,161 +83,147 @@ public class DefaultTransactionalIndexTest extends PersonFixtures {
 	}
 
 	private TransactionalIndex createTransactionalIndex() {
-		return new DefaultTransactionalIndex(new DefaultBulkIndex(new DefaultIndex(es.client(), getClass().getSimpleName().toLowerCase(), Mappings.of(mapper, Person.class))), manager);
+		return new DefaultTransactionalIndex(new DefaultBulkIndex(new DefaultIndex(es.client(), getClass().getSimpleName().toLowerCase(),
+				Mappings.of(mapper, Person.class))), manager);
 	}
-	
+
 	@Test(expected = NotFoundException.class)
 	public void loadingMissingRevision_ShouldThrowNotFoundException() throws Exception {
 		txIndex.loadRevision(Person.class, Branch.MAIN_PATH, PERSON_1_STORAGEKEY);
 	}
-	
+
 	@Test
-	public void whenCommittingFirstRevisionOnMAIN_ThenItShouldBeAvailableInMAINIndex() throws Exception {
+	public void commitOnMAIN_ShouldBeAvailable() throws Exception {
 		final IndexTransaction tx = openTransaction(main);
 		tx.add(PERSON_1_STORAGEKEY, person1_2015);
 		tx.commit(COMMIT_MESSAGE);
-		
-		final Person rev = txIndex.loadRevision(Person.class, Branch.MAIN_PATH, PERSON_1_STORAGEKEY);
-		assertEquals(PERSON_1_STORAGEKEY, rev.getStorageKey());
-		assertEquals(person1_2015, rev);
-	}
-	
-	@Test
-	public void whenCommittingTwoRevisionsOnMAIN_ThenLoadShouldReturnTheModifiedEntity() throws Exception {
-		whenCommittingFirstRevisionOnMAIN_ThenItShouldBeAvailableInMAINIndex();
-		
-		final IndexTransaction tx2 = openTransaction(main);
-		tx2.add(PERSON_1_STORAGEKEY, person1_YobChanged);
-		tx2.commit(COMMIT_MESSAGE);
-		
-		final Person rev = txIndex.loadRevision(Person.class, Branch.MAIN_PATH, PERSON_1_STORAGEKEY);
-		assertEquals(person1_YobChanged, rev);
-	}
-	
-	@Test
-	public void whenCommittingFirstRevisionOnMAIN_AndCreatingEmptyBranch_ThenQueryOnBranchShouldReturnTheRevisionFromMAIN() throws Exception {
-		whenCommittingFirstRevisionOnMAIN_ThenItShouldBeAvailableInMAINIndex();
-		
-		final Branch branchA = createBranch(Branch.MAIN_PATH, "a");
-		final Person rev = txIndex.loadRevision(Person.class, branchA.path(), PERSON_1_STORAGEKEY);
+
+		final Person rev = assertPersonAvailable("MAIN", PERSON_1_STORAGEKEY);
 		assertEquals(person1_2015, rev);
 	}
 
 	@Test
-	public void whenCommittingSecondRevisionOnBranch_ThenQueryOnBranchShouldReturnRevisionOnBranch() throws Exception {
-		whenCommittingFirstRevisionOnMAIN_ThenItShouldBeAvailableInMAINIndex();
-		
-		// open branch
-		final Branch branchA = createBranch(Branch.MAIN_PATH, "a");
-		
-		// make commit on branch
-		final IndexTransaction tx = openTransaction(branchA);
+	public void commitTwiceOnBranch_ShouldReturnLatestRevision() throws Exception {
+		commitOnMAIN_ShouldBeAvailable();
+
+		final IndexTransaction tx = openTransaction(main);
 		tx.add(PERSON_1_STORAGEKEY, person1_YobChanged);
 		tx.commit(COMMIT_MESSAGE);
-		// verify that MAIN version still has yob 2015
-		final Person personMain = txIndex.loadRevision(Person.class, Branch.MAIN_PATH, PERSON_1_STORAGEKEY);
-		assertEquals(person1_2015, personMain);
-		
-		// verify that Branch A has new version with yob 1997
-		final Person personBranch = txIndex.loadRevision(Person.class, branchA.path(), PERSON_1_STORAGEKEY);
-		assertEquals(person1_YobChanged, personBranch);
+
+		final Person rev = assertPersonAvailable("MAIN", PERSON_1_STORAGEKEY);
+		assertEquals(person1_YobChanged, rev);
 	}
-	
+
 	@Test
-	public void whenCommittingOnBranchAndOnMain_ThenQueriesShouldReturnRespectiveVersions() throws Exception {
-		// execute commit on Branch A
-		whenCommittingSecondRevisionOnBranch_ThenQueryOnBranchShouldReturnRevisionOnBranch();
-		
-		// execute commit on MAIN
+	public void emptyBranch_ShouldReturnRevisionFromParent() throws Exception {
+		commitOnMAIN_ShouldBeAvailable();
+		createBranch("MAIN", "a");
+		final Person rev = assertPersonAvailable("MAIN/a", PERSON_1_STORAGEKEY);
+		assertEquals(person1_2015, rev);
+	}
+
+	@Test
+	public void commitOnBranch_ShouldBeAvailable() throws Exception {
+		commitOnMAIN_ShouldBeAvailable();
+		createBranch("MAIN", "a");
+		commitOnBranch_ShouldBeAvailable("MAIN/a");
+		// verify that Branch A has new version with yob 1997
+		final Person personBranch = assertPersonAvailable("MAIN/a", PERSON_1_STORAGEKEY);
+		assertEquals(person1_YobChanged, personBranch);
+		// verify that MAIN version still has yob 2015
+		final Person personMain = assertPersonAvailable("MAIN", PERSON_1_STORAGEKEY);
+		assertEquals(person1_2015, personMain);
+	}
+
+	@Test
+	public void commitOnMAINAndOnBranch_LoadShouldReturnRespectiveVersions() throws Exception {
+		commitOnBranch_ShouldBeAvailable();
+
 		final IndexTransaction tx = openTransaction(main);
 		tx.add(PERSON_1_STORAGEKEY, person1_FirstNameChanged);
 		tx.commit(COMMIT_MESSAGE);
-		
+
 		// verify that MAIN version still has yob 2015, but firstName 'MAIN'
-		final Person personMain = txIndex.loadRevision(Person.class, Branch.MAIN_PATH, PERSON_1_STORAGEKEY);
+		final Person personMain = assertPersonAvailable("MAIN", PERSON_1_STORAGEKEY);
 		assertEquals(person1_FirstNameChanged, personMain);
-		
+
 		// verify that Branch A has new version with yob 1997
-		final Person personBranch = txIndex.loadRevision(Person.class, "MAIN/a", PERSON_1_STORAGEKEY);
+		final Person personBranch = assertPersonAvailable("MAIN/a", PERSON_1_STORAGEKEY);
 		assertEquals(person1_YobChanged, personBranch);
 	}
-	
+
 	@Ignore
 	@Test
 	public void whenRebasingBranch_ThenQueryShouldReturnNewVersionOnBranch() throws Exception {
-		whenCommittingFirstRevisionOnMAIN_AndCreatingEmptyBranch_ThenQueryOnBranchShouldReturnTheRevisionFromMAIN();
-		
+		emptyBranch_ShouldReturnRevisionFromParent();
+
 		// make a change on MAIN, but leave branch unchanged
 		final IndexTransaction tx = openTransaction(main);
 		tx.add(PERSON_1_STORAGEKEY, person1_FirstNameChanged);
 		tx.commit(COMMIT_MESSAGE);
 
 		// verify that person has Foo before rebase
-		final Person personBeforeRebase = txIndex.loadRevision(Person.class, "MAIN/a", PERSON_1_STORAGEKEY);
+		final Person personBeforeRebase = assertPersonAvailable("MAIN/a", PERSON_1_STORAGEKEY);
 		assertEquals(person1_2015, personBeforeRebase);
-		
+
 		// rebase branch should move baseTimestamp forward
 		manager.getBranch("MAIN/a").rebase("Rebased branch A");
 		// verify that person has MAIN after rebase
-		final Person personAfterRebase = txIndex.loadRevision(Person.class, "MAIN/a", PERSON_1_STORAGEKEY);
+		final Person personAfterRebase = assertPersonAvailable("MAIN/a", PERSON_1_STORAGEKEY);
 		assertEquals(person1_FirstNameChanged, personAfterRebase);
 	}
-	
+
 	@Test
-	public void deleteRevision_ShouldThrowNotFoundOnBranch_() throws Exception {
-		whenCommittingFirstRevisionOnMAIN_ThenItShouldBeAvailableInMAINIndex();
-		final IndexTransaction tx = openTransaction(main);
-		tx.delete(PERSON_1_STORAGEKEY, Person.class);
-		tx.commit(COMMIT_MESSAGE);
-		
+	public void deleteRevisionOnBranch_ShouldThrowNotFound() throws Exception {
+		commitOnMAIN_ShouldBeAvailable();
+		deletePerson("MAIN", PERSON_1_STORAGEKEY);
+		assertPersonNotAvailable("MAIN", PERSON_1_STORAGEKEY);
+	}
+
+	@Test
+	public void deleteRevisionOnChild_ShouldThrowNotFoundOnChildButStillAvailableOnParent() throws Exception {
+		commitOnMAIN_ShouldBeAvailable();
+		createBranch("MAIN", "a");
+		deletePerson("MAIN/a", PERSON_1_STORAGEKEY);
+		assertPersonNotAvailable("MAIN/a", PERSON_1_STORAGEKEY);
+		assertPersonAvailable("MAIN", PERSON_1_STORAGEKEY);
+	}
+
+	private Person assertPersonAvailable(String branch, long storageKey) {
 		try {
-			final Person p = txIndex.loadRevision(Person.class, main.path(), PERSON_1_STORAGEKEY);
-			fail("Person is still available after deleting it: " + p);
+			final Person person = txIndex.loadRevision(Person.class, branch, storageKey);
+			assertNotNull("Person revision was empty", person);
+			return person;
+		} catch (NotFoundException e) {
+			throw new AssertionError("Person is not available, branch: " + branch + ", person: " + storageKey, e);
+		}
+	}
+
+	private void deletePerson(final String branchPath, final long storageKey) {
+		final IndexTransaction tx = openTransaction(branchPath);
+		tx.delete(storageKey, Person.class);
+		tx.commit(COMMIT_MESSAGE);
+	}
+
+	private void assertPersonNotAvailable(final String branchPath, final long storageKey) {
+		try {
+			final Person p = txIndex.loadRevision(Person.class, branchPath, storageKey);
+			fail("Person " + storageKey + "is still available on " + branchPath + " after deleting it: " + p);
 		} catch (NotFoundException e) {
 			// expected
 		}
 	}
-	
-//	@Test
-//	public void whenGettingChangesOnBranch_ThenItShouldReturnAllChanges() throws Exception {
-//		whenCommittingSecondRevisionOnBranch_ThenQueryOnBranchShouldReturnRevisionOnBranch();
-//		final IndexTransaction tx = index.transaction("MAIN/a");
-//		assertThat(tx.changes()).hasSize(1);
-//	}
 
-//	@Test
-//	public void whenGettingMultipleChangesOnBranch_ThenItShouldReturnOnlyTheLatestOnePerComponent() throws Exception {
-//		whenCommittingSecondRevisionOnBranch_ThenQueryOnBranchShouldReturnRevisionOnBranch();
-//		// new commit on component 1
-//		final IndexTransaction tx = index.transaction("MAIN/a");
-//		Person person = (Person) tx.get(PERSONS, PERSON_1_ID);
-//		person.setFirstName("BranchName");
-//		tx.index(person);
-//		tx.commit();
-//		
-//		final Collection<Component> changes = tx.changes();
-//		assertThat(changes).hasSize(1);
-//		final Person personLatestOnBranch = (Person) Iterables.getFirst(changes, null);
-//		assertEquals("BranchName", personLatestOnBranch.getFirstName());
-//	}
-//	
-//	@Test
-//	public void test_LargeChangeSet_Commit() throws Exception {
-//		final int changeSetSize = 1_000_000;
-//		final IndexTransaction tx = index.transaction(RevisionIndex.MAIN_BRANCH);
-//		Stopwatch watch = Stopwatch.createStarted();
-//		for (int i = 0; i < changeSetSize; i++) {
-//			tx.index(createRandomPerson(i));
-//		}
-//		tx.commit();
-//		System.out.println("TX Commit of 1_000_000 elements took: " + watch);
-//		watch.reset();
-//		watch.start();
-//		final Collection<Component> changes = tx.changes();
-//		System.out.println("ChangeSet computation from index took: " + watch);
-//		assertThat(changes).hasSize(changeSetSize);
-//	}
-	
+	private void commitOnBranch_ShouldBeAvailable(String branchPath) {
+		final IndexTransaction tx = openTransaction(branchPath);
+		tx.add(PERSON_1_STORAGEKEY, person1_YobChanged);
+		tx.commit(COMMIT_MESSAGE);
+	}
+
+	private IndexTransaction openTransaction(final String branch) {
+		return openTransaction(manager.getBranch(branch));
+	}
+
 	private IndexTransaction openTransaction(final Branch branch) {
 		final int commitId = commitIdProvider.incrementAndGet();
 		final long commitTimestamp = clock.incrementAndGet();
@@ -246,9 +232,9 @@ public class DefaultTransactionalIndexTest extends PersonFixtures {
 
 			@Override
 			public <T extends Component> void delete(long storageKey, Class<T> type) {
-				original.delete(storageKey, type);				
+				original.delete(storageKey, type);
 			}
-			
+
 			@Override
 			public void commit(String commitMessage) {
 				original.commit(commitMessage);
@@ -260,17 +246,17 @@ public class DefaultTransactionalIndexTest extends PersonFixtures {
 			public void add(long storageKey, Component object) {
 				original.add(storageKey, object);
 			}
-			
+
 			@Override
 			public String branch() {
 				return original.branch();
 			}
-			
+
 		};
 	}
-	
+
 	private Branch createBranch(String parent, String name) {
 		return manager.getBranch(parent).createChild(name);
 	}
-	
+
 }
