fb7022f19e5bbdb2fb0063708ffb40f6b0156475
CMark <mczotter@b2international.com>
2015-07-16 12:48:42 +0200

    SO-1639: use new VisibleInBranchManager in SnomedQueryTest

--- snomed/snowowl-snomed-core-tests/src/com/b2international/snowowl/snomed/core/io/SnomedQueryTest.java
+++ snomed/snowowl-snomed-core-tests/src/com/b2international/snowowl/snomed/core/io/SnomedQueryTest.java
@@ -15,11 +15,8 @@
  */
 package com.b2international.snowowl.snomed.core.io;
 
-import static com.google.common.collect.Lists.newLinkedList;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -29,23 +26,20 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import com.b2international.snowowl.core.DefaultObjectMapper;
-import com.b2international.snowowl.core.branch.Branch;
-import com.b2international.snowowl.core.internal.branch.IndexBranchManagerImpl;
 import com.b2international.snowowl.core.internal.branch.InternalBranch;
+import com.b2international.snowowl.core.internal.branch.VisibleInBranchManagerImpl;
 import com.b2international.snowowl.core.store.Store;
 import com.b2international.snowowl.core.store.index.DefaultBulkIndex;
 import com.b2international.snowowl.core.store.index.DefaultIndex;
 import com.b2international.snowowl.core.store.index.Index;
 import com.b2international.snowowl.core.store.index.IndexStore;
 import com.b2international.snowowl.core.store.index.Mappings;
-import com.b2international.snowowl.core.store.index.MultiIndex;
 import com.b2international.snowowl.core.store.index.tx.DefaultTransactionalIndex;
 import com.b2international.snowowl.core.store.index.tx.TransactionalIndex;
 import com.b2international.snowowl.snomed.core.store.index.Concept;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.base.Stopwatch;
-import com.google.common.collect.Iterables;
 import com.google.common.io.Resources;
 
 /**
@@ -56,14 +50,17 @@ public class SnomedQueryTest {
 	private static final String SETTINGS_FILE = "snomed_settings.json";
 	
 	@ClassRule
-	public static final ESTransportClientRule rule = new ESTransportClientRule("mczotter", "localhost", 9300);
+	public static final ESTransportClientRule rule = new ESTransportClientRule("mczotter");
 	
 	private Index index;
 	private Store<InternalBranch> branchStore;
-	private IndexBranchManagerImpl branching;
+	private VisibleInBranchManagerImpl branching;
 	
 	private ObjectMapper mapper = new DefaultObjectMapper();
 	private AtomicLong clock = new AtomicLong(0L);
+
+	private TransactionalIndex txIndex;
+	private SnomedBrowser browser;
 	
 	@Before
 	public void givenIndex() throws Exception {
@@ -71,15 +68,17 @@ public class SnomedQueryTest {
 		
 		this.index = new DefaultIndex(rule.client(), "snomed_ct", new Mappings(mapper));
 		this.branchStore = new IndexStore<>(index, InternalBranch.class);
-		this.branching = new IndexBranchManagerImpl(branchStore, clock.incrementAndGet(), "snomed_ct", rule.client(), Mappings.of(mapper, Concept.class), settings, clock);
+		this.branching = new VisibleInBranchManagerImpl(branchStore, clock);
+		this.txIndex = new DefaultTransactionalIndex(new DefaultBulkIndex(index), branching);
+		this.browser = new SnomedBrowser(txIndex);
+		this.branching.setIndex(txIndex);
 	}
 	
 	@Test
 	public void queryChildrenOfRootOnMAIN() throws Exception {
 		final String branch = "MAIN";
-		final SnomedBrowser browser20150131 = new SnomedBrowser(createTransactionalIndex(branch));
 		final Stopwatch watch = Stopwatch.createStarted();
-		final Iterable<Concept> result = browser20150131.getChildren(branch, "138875005");
+		final Iterable<Concept> result = browser.getChildren(branch, "138875005");
 		System.out.println("RootChildren took: " + watch);
 		assertThat(result).hasSize(19);
 	}
@@ -88,23 +87,10 @@ public class SnomedQueryTest {
 	@Test
 	public void queryChildrenOfClinicalFindingOnMAIN() throws Exception {
 		final String branch = "MAIN";
-		final SnomedBrowser browser = new SnomedBrowser(createTransactionalIndex(branch));
 		final Stopwatch watch = Stopwatch.createStarted();
 		final Iterable<Concept> result = browser.getDescendants(branch, "404684003");
 		System.out.println("ClinicalFinding Descendants took: " + watch);
-		System.out.println(Iterables.size(result));
-//		for (Concept concept : result) {
-//			System.out.println(concept);
-//		}
-	}
-	
-	private TransactionalIndex createTransactionalIndex(String path) {
-		return createTransactionalIndex(branching.getBranch(path));
-	}
-	
-	private TransactionalIndex createTransactionalIndex(Branch branch) {
-		final LinkedList<String> indexes = newLinkedList(branch.metadata().get(IndexBranchManagerImpl.BRANCH_INDEXES, List.class));
-		return new DefaultTransactionalIndex(new DefaultBulkIndex(new MultiIndex(rule.client(), Mappings.of(mapper, Concept.class), null, indexes)), branching);
+		assertThat(result).hasSize(102217);
 	}
 	
 }
